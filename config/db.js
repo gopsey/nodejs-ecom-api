1b3c4a20149cf1e4af30f8ea9a7045565aada412
Gopalakrishnan <gopalkris96@gmail.com>
2024-07-10 23:52:30 +0530

    updated mongodb atlas scripts for mongo server deployment

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ dotenv.config({ path: './config.env' })
 
 const connectDB = async () => {
    //U: gopsey P: Y78yQBMNc2UHX4oy
-   const connection = await mongoose.connect('mongodb+srv://gopsey:Y78yQBMNc2UHX4oy@clusterecommern.wlkbzaf.mongodb.net/ecommern?retryWrites=true&w=majority&appName=ClusterEcomMern') //process.env.MONGO_URI
+   const connection = await mongoose.connect(process.env.MONGO_URI)
    console.log(`MongoDB connected at: ${connection.connection.host}:${connection.connection.port}`)
 }
 
